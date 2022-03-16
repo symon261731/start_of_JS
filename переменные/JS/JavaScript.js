d6e21c178bc2bcd9e6fccdb6e3fc025df79749d5
symon261731 <99682209+symon261731@users.noreply.github.com>
2022-03-16 05:19:04 +0500

    Add files via upload

---
+++ переменные/JS/JavaScript.js
@@ -0,0 +1,22 @@
+
+
+// ПЕРЕМЕННЫЕ
+
+//var winners = 25; // Числовое значение
+//var name = "Simon"  // Строковое значение
+//var isEnglish = false; // Логическое значения не заключаются в кавычки
+//var secondName // переменная без присваивания
+/*
+var joke = "JavaScript walked into a bar...";
+var toldJoke = false;
+var $punchline = "better watch out for those semi-colons";
+var entage = 20;
+var result;
+
+if (toldJoke == true) {
+    alert($punchline);
+}
+else {
+    alert(joke);
+}
+*/
