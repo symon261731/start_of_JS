79186dfcb0c8c6bc166655dc8696b965f57b1fff
symon261731 <99682209+symon261731@users.noreply.github.com>
2022-03-16 17:35:17 +0500

    expressions

---
+++ выражения/js/javascript.js
@@ -0,0 +1,30 @@
+// ВЫРАЖЕНИЯ
+var temp = 10;
+total = (9 / 5) * temp + 32;
+alert(total);
+
+//var color = "orange";
+//alert(color == "pink" == true);
+
+//
+var name = "Martha";
+const result = name + "," + "you've won!";
+alert(result);
+
+
+//
+var yourLevel = 7;
+alert(yourLevel > 5 == true);
+//
+var level = 5;
+var points = 30000;
+var bonus = 3300;
+total = (level * points) + bonus;
+alert(total);
+
+var color = "orange";
+alert(color != "orange" == true); // ОПЕРАТОР != ПРОВЕРЯЕТ, ЧТО ДВА ЗНАЧЕНИЯ НЕ РАВНЫ 
+
+
+total = 1000 + "108";
+alert(total);
